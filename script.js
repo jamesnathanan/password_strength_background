62dc36da93bf1225f9d1321d885176b39a7eadec
jamesnathanan <artofgems@live.com>
2021-09-06 06:46:45 +0700

    create eventlistener for blur effect depending on password length

---
+++ script.js
@@ -0,0 +1,12 @@
+const password = document.getElementById("password");
+const background = document.getElementById("background");
+
+password.addEventListener("input", (e) => {
+  const input = e.target.value;
+  const length = input.length;
+
+  let blurValue = 20 - length * 2;
+  blurValue <= 0 ? (blurValue = 0) : null;
+  background.style.filter = `blur(${blurValue}px)`;
+  console.log(blurValue);
+});
